192589e967b2620a4c830816bbc4c0ac1671e67a
mgola12 <106336274+mgola12@users.noreply.github.com>
2023-07-04 10:54:59 -0700

    Add files via upload

---
+++ build/check.C
@@ -0,0 +1,83 @@
+
+*************************************************************
+ Geant4 version Name: geant4-10-01-patch-03    (5-February-2016)
+                      Copyright : Geant4 Collaboration
+                      Reference : NIM A 506 (2003), 250-303
+                            WWW : http://cern.ch/geant4
+*************************************************************
+
+Checking overlaps for volume glassTube ... OK! 
+Checking overlaps for volume pmtSphere ... OK! 
+The solids are touching each other with no space
+Checking overlaps for volume glassInnerSphere ... OK! 
+Checking overlaps for volume absorber ... OK! 
+PMT SURFACE == 2
+CATHODE PARAMETER == 0
+Use SK cathode optical parameters
+Scoring volume:  logicabsorber
+Checking overlaps for volume placeLayer ... OK! 
+Process Name: Transportation
+Process Name: OpAbsorption
+Process Name: OpRayleigh
+Process Name: OpMieHG
+Process Name: OpBoundary
+Visualization Manager instantiating with verbosity "warnings (3)"...
+Visualization Manager initialising...
+Registering graphics systems...
+
+You have successfully registered the following graphics systems.
+Current available graphics systems are:
+  ASCIITree (ATree)
+  DAWNFILE (DAWNFILE)
+  G4HepRep (HepRepXML)
+  G4HepRepFile (HepRepFile)
+  OpenGLImmediateQt (OGLI, OGLIQt)
+  OpenGLStoredQt (OGL, OGLS, OGLSQt)
+  RayTracer (RayTracer)
+  RayTracerX (RayTracerX)
+  VRML1FILE (VRML1FILE)
+  VRML2FILE (VRML2FILE)
+  gMocrenFile (gMocrenFile)
+
+Registering model factories...
+
+You have successfully registered the following model factories.
+Registered model factories:
+  generic
+  drawByCharge
+  drawByParticleID
+  drawByOriginVolume
+  drawByAttribute
+
+Registered filter factories:
+  chargeFilter
+  particleFilter
+  originVolumeFilter
+  attributeFilter
+
+You have successfully registered the following user vis actions.
+Run Duration User Vis Actions: none
+End of Event User Vis Actions: none
+End of Run User Vis Actions: none
+
+Some /vis commands (optionally) take a string to specify colour.
+Available colours:
+  black, blue, brown, cyan, gray, green, grey, magenta, red, white, yellow
+
+Setting water attenuation parameter 1.300000
+Setting cathode reflectivity parameter 0.320000
+Setting Rayleigh scattering parameter 0.750000
+Setting blacksheet reflection parameter 2.500000
+Setting Mie scattering parameter 0.000000
+Setting Top Veto Off
+Setting Top Veto PMT Spacing 100.000000
+Setting PMT photocathode surface optical model as Model 2 (0 means default dielectric model)
+Setting PMT photocathode surface parameters as Choice 0 (0 = SK, 1 = KCsRb, 2 = RbCsCb)
+G4 kernel has come to Quit state.
+================== Deleting memory pools ===================
+Number of memory pools allocated: 3; of which, static: 0
+Dynamic pools deleted: 3 / Total memory freed: 0.0029 Mb
+============================================================
+RunManagerKernel is deleted. Good bye :)
+Graphics systems deleted.
+Visualization Manager deleting...
